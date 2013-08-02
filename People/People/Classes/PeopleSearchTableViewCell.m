978b0ec6882aa00b3ee87eab472bc4641208af70
Christian <christian.fsampaio@gmail.com>
2013-08-01 21:00:15 -0300

    Implementing cell menu buttons on search screen

--- People/People/Classes/PeopleSearchTableViewCell.m
+++ People/People/Classes/PeopleSearchTableViewCell.m
@@ -7,29 +7,49 @@
 //
 
 #import "PeopleSearchTableViewCell.h"
+#import "PeopleColaborador.h"
+#import "PeopleBasicTheme.h"
 #import <PeopleSwipeTableViewCell/MCSwipeTableViewCell.h>
+
+@interface PeopleSearchTableViewCell()
+
+@property (weak, nonatomic) IBOutlet UILabel *collaboratorNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *collaboratorRoleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *collaboratorPhoneLabel;
+@property (weak, nonatomic) IBOutlet UILabel *collaboratorLoginLabel;
+@property (weak, nonatomic) IBOutlet UILabel *collaboratorCellphoneLabel;
+
+@end
 
 @implementation PeopleSearchTableViewCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+- (void)updateLabelsWithCollaborator:(PeopleColaborador *)collaborator
 {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
-- (void)awakeFromNib
-{
+    self.collaboratorNameLabel.text = collaborator.name;
+    self.collaboratorRoleLabel.text = collaborator.role;
+    self.collaboratorPhoneLabel.text = collaborator.phone;
+    self.collaboratorLoginLabel.text = collaborator.login;
+    self.collaboratorCellphoneLabel.text = collaborator.mobile;
     
+    [self addButtons];
+}
+
+- (void)addButtons
+{
+    self.leftMenuColor = [PeopleBasicTheme peopleColor2Secondary];
+    [self addButton:[self buttonForMenuWithImageName:@"ico-swipe-phone-normal"] toCellSide:MCSwipeTableViewCellSideLeft];
+    [self addButton:[self buttonForMenuWithImageName:@"ico-swipe-mobile-normal"] toCellSide:MCSwipeTableViewCellSideLeft];
+    [self addButton:[self buttonForMenuWithImageName:@"ico-swipe-mail-normal"] toCellSide:MCSwipeTableViewCellSideLeft];
+    [self addButton:[self buttonForMenuWithImageName:@"ico-swipe-sms-normal"] toCellSide:MCSwipeTableViewCellSideLeft];
+}
+
+- (UIButton *)buttonForMenuWithImageName:(NSString *)imageName
+{
+    UIImage *buttonImage = [UIImage imageNamed:imageName];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    button.frame = (CGRect){CGPointZero, buttonImage.size};
+    return button;
 }
 
 @end
