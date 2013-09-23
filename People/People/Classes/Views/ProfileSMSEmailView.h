358249c92e0ab089f2e8a50aca2c426266f40d1b
Bruno Koga <koga@brunokoga.com>
2013-09-22 20:49:45 -0500

    More profile view

---
+++ People/People/Classes/Views/ProfileSMSEmailView.h
@@ -0,0 +1,13 @@
+//
+//  ProfileSMSEmailView.h
+//  People
+//
+//  Created by Bruno Koga on 9/22/13.
+//  Copyright (c) 2013 CI&T. All rights reserved.
+//
+
+#import "BKXibView.h"
+
+@interface ProfileSMSEmailView : BKXibView
+
+@end
